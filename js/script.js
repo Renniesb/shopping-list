4617c690ec8c8a2a26ec718fcfb412c195c5c40d
Rennie Bevineau <rsbevineau@gmail.com>
2020-03-28 23:37:00 -0400

    initial js file commit

---
+++ js/script.js
@@ -0,0 +1,11 @@
+function addItemToList() {
+  // a function to add the item to the list
+}
+
+function checkItemOffList() {
+  // a function to check the item off the list
+}
+
+function deleteItem() {
+  // a function to delete the item
+}
